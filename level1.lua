48461b4d27d82939cd5200b9f95eb7392c6165bb
Dmitry Kutuzov <kutuzov@playform.com>
2014-04-06 16:00:43 +0400

    high res movings

--- level1.lua
+++ level1.lua
@@ -5,15 +5,15 @@ local EnemiesCreator = require('enemiescreator')
 local lvl = display.newGroup( )
 
 local levelConfig = {
-	{timeout = 500, num = 3, startPosition = {570, -150}, enemyType = 2, moving = movings.eightTRtoDL, startTimeout = 300, time = 4000 }, 
-{timeout = 5400, num = 3, startPosition = {95, -125}, enemyType = 2, moving = movings.invertX(movings.turnTLtoDR), startTimeout = 300, time = 4000 }, 
-{timeout = 11250, num = 2, startPosition = {70, -150}, enemyType = 2, moving = movings.invertX(movings.eightTRtoDL), startTimeout = 300, time = 4000 }, 
-{timeout = 13850, num = 2, startPosition = {-160, 80}, enemyType = 1, moving = movings.invertX(movings.zigzagTLtoDR), startTimeout = 300, time = 5000 }, 
-{timeout = 19550, num = 2, startPosition = {95, -125}, enemyType = 1, moving = movings.invertX(movings.turnTLtoDR), startTimeout = 300, time = 4000 }, 
-{timeout = 26100, num = 3, startPosition = {-160, 80}, enemyType = 1, moving = movings.invertX(movings.zigzagTLtoDR), startTimeout = 300, time = 5000 }, 
-{timeout = 31200, num = 2, startPosition = {-160, 80}, enemyType = 2, moving = movings.invertX(movings.zigzagTLtoDR), startTimeout = 300, time = 5000 }, 
-{timeout = 34700, num = 3, startPosition = {-160, 80}, enemyType = 2, moving = movings.invertX(movings.zigzagTLtoDR), startTimeout = 300, time = 5000 }, 
-{timeout = 38850, num = 2, startPosition = {95, -125}, enemyType = 2, moving = movings.invertX(movings.turnTLtoDR), startTimeout = 300, time = 4000 }, 
+{timeout = 1100, num = 3, startPosition = {800, 80}, enemyType = 1, moving = movings.zigzagTLtoDR, startTimeout = 500, time = 10000 }, 
+{timeout = 5000, num = 3, startPosition = {80, -50}, enemyType = 2, moving = movings.zigzagTtoD, startTimeout = 500, time = 8000 }, 
+{timeout = 9000, num = 3, startPosition = {575, -120}, enemyType = 1, moving = movings.roundD, startTimeout = 500, time = 8000 }, 
+{timeout = 14350, num = 2, startPosition = {835, 40}, enemyType = 2, moving = movings.zigzagTRtoTL, startTimeout = 500, time = 12000 }, 
+{timeout = 19850, num = 2, startPosition = {545, -125}, enemyType = 2, moving = movings.turnTLtoDR, startTimeout = 500, time = 5000 }, 
+{timeout = 25950, num = 3, startPosition = {550, -140}, enemyType = 1, moving = movings.roundTRtoDR, startTimeout = 500, time = 8000 }, 
+{timeout = 31000, num = 2, startPosition = {570, -150}, enemyType = 2, moving = movings.eightTRtoDL, startTimeout = 500, time = 8000 }, 
+{timeout = 35900, num = 2, startPosition = {800, 80}, enemyType = 1, moving = movings.zigzagTLtoDR, startTimeout = 500, time = 10000 }, 
+{timeout = 39150, num = 2, startPosition = {80, -50}, enemyType = 1, moving = movings.zigzagTtoD, startTimeout = 500, time = 8000 }, 
 
 }
 
